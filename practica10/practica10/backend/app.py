3a0879c6e6084f13f872572fe510e320057a7f3b
Gutierrez458 <hgeduardo87@gmail.com>
2026-03-11 17:06:57 -0600

    update

--- practica10/practica10/backend/app.py
+++ practica10/practica10/backend/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 CORS(app)
 
 cliente = MongoClient("mongodb://localhost:27017/")
-db = cliente["videojuegosDB"]
+db = cliente["videojuegos_db"]
 coleccion = db["games"]
 
 # OBTENER TODOS
